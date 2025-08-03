618b27f41403ff434c85098198cf45961bb39025
xerotropy <xerotropy@gmail.com>
2025-08-03 03:48:24 +0200

    fixing package export

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,4 +2,4 @@ export * from './button';
 export * from './card';
 export * from './code';
 export * from './lib/utils';
-export { LimelightNav } from './limelight-nav';
+export * from './limelight-nav';
